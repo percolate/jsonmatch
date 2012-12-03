08d12d8adf7a1805e6e7a296978070bcc7dcf09d
James O'Beirne <james.obeirne@gmail.com>
2012-12-03 17:22:05 -0500

    packaging for pypi

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='jsonmatch',
+    version='0.1',
+    author="James O'Beirne",
+    author_email='jamesob@percolate.com',
+    packages=['jsonmatch'],
+    url='https://github.com/percolate/jsonmatch',
+    license='see LICENCE.txt',
+    description='A flexible framework for testing JSON dicts against schemas.',
+    zip_safe=False,
+)
+
